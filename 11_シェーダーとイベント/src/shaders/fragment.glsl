c2e119d438fde390bcae102837126cf46dfe6be1
hirochan9854 <ito.hiro9854@gmail.com>
2024-08-02 13:51:00 +0900

    シェーダーとイベント

--- 11_シェーダーとイベント/src/shaders/fragment.glsl
+++ 11_シェーダーとイベント/src/shaders/fragment.glsl
@@ -1,4 +1,7 @@
-void main()
-{
+uniform float r;
+uniform float g;
+uniform float b;
 
+void main(){
+  gl_FragColor = vec4(r, g, b, 1.0); //色の定義(赤、緑、青、不透明度)
 }
--- 11_シェーダーとイベント/src/shaders/vertex.glsl
+++ 11_シェーダーとイベント/src/shaders/vertex.glsl
@@ -1,4 +1,20 @@
+uniform float uFrequency;
+uniform float uTime;
+uniform float uMove;
+uniform float uSide; //追加
+
 void main()
 {
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
+  // z座標の変位をsin波で制御
+  modelPosition.z += sin(modelPosition.x * uFrequency + uTime) * 0.1; //変更
+
+  // x座標の変位をsin波で制御
+  modelPosition.x += sin(modelPosition.z * uFrequency + uMove) * 0.1; //追加
+
+  // y座標の変位をsin波で制御
+  modelPosition.y += sin(modelPosition.z * uFrequency + uSide) * 0.1; //追加
+
+  gl_Position = projectionMatrix * viewMatrix * modelPosition;
 }
